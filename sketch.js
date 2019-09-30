e0bd33eb3d1627e195d206d1cf30f8294e19f155
beaforesti <bea_fiore@live.it>
2019-09-30 23:07:38 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,47 +1,71 @@
-var col1 = '#581845';
-var col2 = '#900c3f';
-var col3 = '#c70039';
-var col4 = '#ff5733';
-var col5 = '#ffc30f';
+//sunrise colours
+var col1 = '#022660';
+var col2 = '#0d469d';
+var col3 = '#6d54a9';
+var col4 = '#ce75c2';
+var col5 = '#db907d';
 
-var colorList = [col3, col4, col5];
+var colorList = [col1, col2, col3, col4, col5];
 
+//sunset colours
+var colS1 = '#381c2a';
+var colS2 = '#3c3b5f';
+var colS3 = '#f40001';
+var colS4 = '#f48740';
+var colS5 = '#ffd400';
+
+var colorListS = [colS1, colS2, colS3, colS4, colS5];
 
 function preload() {
   // put preload code here
 }
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  background(col5)
+  createCanvas(windowWidth, windowHeight)
+  background(255);
   noStroke();
-  angleMode(DEGREES);
-
+  frameRate(12);
 }
 
 function draw() {
-  var x = random() * windowWidth;
-  var y = random() * windowHeight;
-  fill(lerpColor(color(col3), color(col5), frameCount/100));
-  quad(x, y, x + 100, y, x+ 100, y+100, x, y+100);
 
-
-  // triangles
   for (var t = 0; t < windowWidth; t += 100) {
     for (var u = 0; u < windowHeight; u += 100) {
 
-      fill(color(col1));
-      triangle(t, u, t + 100, u, t + 50, u + 50);
-      fill(color(col2));
-      triangle(t, u + 100, t + 100, u + 100, t + 50, u + 50);
+      if (frameCount < 100) {
+        // text
+        push();
+        var font = 'Colours from Sunrise to Sunset';
+        fill(255);
+        noStroke();
+        textSize(25);
+        textAlign(CENTER, CENTER);
+        text(font, width / 2, height / 5);
+        pop();
 
-      if (t < windowWidth / 3 && u < windowHeight / 3 || t > windowWidth / 3 && u > windowHeight / 3) {
-        fill(lerpColor(color(col1), color(col4), frameCount/100));
-        quad(t, u + 50, t + 50, u, t + 100, u + 50, t + 50, u + 100);
-      } else if (t > windowWidth / 3 && u < windowHeight / 3 || t < windowWidth/3 && u > windowHeight/3) {
-        fill(lerpColor(color(col1), color(col3), frameCount/100));
-        quad(t, u + 50, t + 50, u, t + 100, u + 50, t + 50, u + 100);
+        //from sunrise to sunset
+        fill(lerpColor(color(col1), color(colS1), frameCount / 100));
+        triangle(t, u, t + 100, u, t + 50, u + 50);
+        fill(lerpColor(color(col2), color(colS2), frameCount / 100));
+        triangle(t, u, t + 50, u + 50, t - 50, u + 50);
+        fill(lerpColor(color(col3), color(colS3), frameCount / 100));
+        triangle(t, u + 100, t + 100, u + 100, t + 50, u + 50);
+        fill(lerpColor(color(col4), color(colS4), frameCount / 100));
+        triangle(t, u + 50, t + 50, u + 50, t, u + 100);
+        fill(lerpColor(color(col5), color(colS5), frameCount / 100));
+        triangle(t + 50, u + 50, t + 100, u + 50, t + 100, u + 100);
+      } else if (frameCount > 100) {
 
+        // crazy colours sunrise and sunset
+        var index = floor(random() * colorList.length);
+        var colorHex = colorList[index];
+        fill(color(colorHex));
+        triangle(t, u, t + 100, u, t + 50, u + 50);
+
+        var indexS = floor(random() * colorListS.length);
+        var colorHexS = colorListS[indexS];
+        fill(color(colorHexS));
+        triangle(t, u, t + 50, u + 50, t - 50, u + 50);
       }
     }
   }
