eff7ae2b9b45a7b80bd86a9440a37d6c39bfc1ae
beaforesti <bea_fiore@live.it>
2019-10-02 00:55:23 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,20 +1,21 @@
 //sunrise colours
-var col1 = '#022660';
-var col2 = '#0d469d';
-var col3 = '#6d54a9';
+var col1 = '#db907d';
+var col2 = '#6d54a9';
+var col3 = '#022660';
 var col4 = '#ce75c2';
-var col5 = '#db907d';
+var col5 = '#0d469d';
 
 var colorList = [col1, col2, col3, col4, col5];
 
 //sunset colours
-var colS1 = '#381c2a';
-var colS2 = '#3c3b5f';
-var colS3 = '#f40001';
+var colS1 = '#ffd400';
+var colS2 = '#f40001';
+var colS3 = '#381c2a';
 var colS4 = '#f48740';
-var colS5 = '#ffd400';
+var colS5 = '#3c3b5f';
 
 var colorListS = [colS1, colS2, colS3, colS4, colS5];
+
 
 function preload() {
   // put preload code here
@@ -24,7 +25,7 @@ function setup() {
   createCanvas(windowWidth, windowHeight)
   background(255);
   noStroke();
-  frameRate(12);
+  frameRate(25);
 }
 
 function draw() {
@@ -33,30 +34,32 @@ function draw() {
     for (var u = 0; u < windowHeight; u += 100) {
 
       if (frameCount < 100) {
-        // text
-        push();
-        var font = 'Colours from Sunrise to Sunset';
-        fill(255);
-        noStroke();
-        textSize(25);
-        textAlign(CENTER, CENTER);
-        text(font, width / 2, height / 5);
-        pop();
 
         //from sunrise to sunset
         fill(lerpColor(color(col1), color(colS1), frameCount / 100));
         triangle(t, u, t + 100, u, t + 50, u + 50);
         fill(lerpColor(color(col2), color(colS2), frameCount / 100));
-        triangle(t, u, t + 50, u + 50, t - 50, u + 50);
+        triangle(t + 50, u + 50, t + 100, u, t + 100, u + 100);
         fill(lerpColor(color(col3), color(colS3), frameCount / 100));
         triangle(t, u + 100, t + 100, u + 100, t + 50, u + 50);
         fill(lerpColor(color(col4), color(colS4), frameCount / 100));
-        triangle(t, u + 50, t + 50, u + 50, t, u + 100);
-        fill(lerpColor(color(col5), color(colS5), frameCount / 100));
-        triangle(t + 50, u + 50, t + 100, u + 50, t + 100, u + 100);
-      } else if (frameCount > 100) {
+        triangle(t, u, t + 50, u + 50, t, u + 100);
+
+        // text
+        fill(255);
+        noStroke();
+        textSize(20);
+        textAlign(CENTER, CENTER);
+        text('Wait till the Sunset colours', width / 2, 50);
+
+      } else if (frameCount > 110) {
+        //text
+        noStroke();
+        textSize(40);
+        text('Click to stop crazy colours', width / 2, height / 2);
 
         // crazy colours sunrise and sunset
+
         var index = floor(random() * colorList.length);
         var colorHex = colorList[index];
         fill(color(colorHex));
@@ -65,8 +68,12 @@ function draw() {
         var indexS = floor(random() * colorListS.length);
         var colorHexS = colorListS[indexS];
         fill(color(colorHexS));
-        triangle(t, u, t + 50, u + 50, t - 50, u + 50);
+        triangle(t, u + 100, t + 100, u + 100, t + 50, u + 50);
       }
     }
   }
+}
+
+function mousePressed() {
+  noLoop();
 }
